7c2be952180efdf4b3a7cf5da13ca8755fab9506
Jason Clarke <jason.clarke@eu.weatherford.com>
2019-02-14 15:47:49 +0000

    First Version

---
+++ FaultFileSorter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FaultDataSort
+{
+    public class FaultFileSorter
+    {
+        public static void Sort(string sourceFile, string destFile)
+        {
+            FaultFileSorter s = new FaultFileSorter(sourceFile,destFile);
+        }
+
+        private FaultFileSorter(string sourceFile, string destFile)
+        {
+            List<string>  allLines = File.ReadAllLines(sourceFile).ToList();
+
+            using (StreamWriter outputFile = new StreamWriter(destFile))
+            {
+                while (allLines.Count > 0)
+                {
+                    string lineToProcess = allLines[0];
+                    allLines.RemoveAt(0);
+
+                    CopyLinesToOutput(outputFile, lineToProcess);
+
+                    string currentTag = GetLineTag(lineToProcess);
+
+                    CopyAllLinesWithTagToOutput(outputFile, allLines, currentTag);
+                }
+            }
+        }
+
+        private void CopyAllLinesWithTagToOutput(StreamWriter outputFile, List<string> lines, string currentTag)
+        {
+            foreach (string line in lines)
+            {
+                string tag = GetLineTag(line);
+                if (tag == currentTag)
+                {
+                    CopyLinesToOutput(outputFile, line);
+                }
+            }
+            lines.RemoveAll(l => GetLineTag(l) == currentTag);
+        }
+
+        private void CopyLinesToOutput(StreamWriter outputFile, string lineToProcess)
+        {
+            outputFile.WriteLine(lineToProcess);
+        }
+
+        private string GetLineTag(string lineText)
+        {
+            string[] elements = lineText.Split(new []{' '}, StringSplitOptions.RemoveEmptyEntries);
+            if (elements.Length < 7) return null;
+            string tag = elements[7].Substring(1); // strip off the first character of the tag
+            return tag;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FaultDataSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Arguments are [Source File] [Destination File]");
+                return;
+            }
+
+            string sourceFile = args[0];
+            string destFile = args[1];
+
+            if (!File.Exists(sourceFile))
+            {
+                Console.WriteLine($"Source File: {sourceFile} not found.");
+                return;
+            }
+
+            if (File.Exists(destFile))
+            {
+                Console.WriteLine($"Destination File: {destFile} already exists.");
+                return;
+            }
+
+            FaultFileSorter.Sort(sourceFile,destFile);
+
+        }
+
+    }
+}
